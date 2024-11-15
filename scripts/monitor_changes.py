329c81ac4fe7c0d11abfe3181d0c684a470043eb
studio-larslo <studio@wordpress-larslo.de>
2024-11-15 17:36:40 +0100

    test mail

--- scripts/monitor_changes.py
+++ scripts/monitor_changes.py
@@ -1,14 +1,6 @@
-from github import Github
-import os
-import re
-import smtplib
 from email.message import EmailMessage
-
-# GitHub configuration
-github_token = os.environ['MONITOR_TOKEN']
-target_repo = "WordPress/gutenberg"
-pattern = r'packages/block-library/src/.*\.scss$'  # Matches SCSS files in any subfolder under src/
-
+import smtplib
+import os
 
 # Email configuration
 smtp_server = os.environ['SMTP_SERVER']
@@ -17,30 +9,11 @@ sender_email = os.environ['SENDER_EMAIL']
 receiver_email = os.environ['RECEIVER_EMAIL']
 email_password = os.environ['EMAIL_PASSWORD']
 
-def check_changes():
-    g = Github(github_token)
-    repo = g.get_repo(target_repo)
-    
-    # Get latest commits
-    commits = repo.get_commits()
-    latest_commit = commits[0]
-    
-    # Check modified files
-    matching_files = []
-    for file in latest_commit.files:
-        if re.search(pattern, file.filename):
-            matching_files.append(file.filename)
-    
-    if matching_files:
-        send_notification(matching_files, latest_commit)
-
-def send_notification(files, commit):
+def send_test_email():
     msg = EmailMessage()
-    msg.set_content(f"Changes detected in files:\n\n" + 
-                    "\n".join(files) + 
-                    f"\n\nCommit: {commit.html_url}")
+    msg.set_content("This is a test email from the monitoring system.")
     
-    msg['Subject'] = 'Repository Change Alert'
+    msg['Subject'] = 'Test Email - Repository Monitor'
     msg['From'] = sender_email
     msg['To'] = receiver_email
     
@@ -50,4 +23,4 @@ def send_notification(files, commit):
         server.send_message(msg)
 
 if __name__ == "__main__":
-    check_changes()
+    send_test_email()
